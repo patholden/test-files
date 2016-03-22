96ac4260b76d9a8ac88632c66d24b17c36d85091
Pat Holden <patholdn@gmail.com>
2016-03-22 16:41:32 -0400

    fixed to compile & link but needs overhaul to work with new api.  Changes to be committed: 	deleted:    laserguide.h 	modified:   xy_io.c

--- xy_io.c
+++ xy_io.c
@@ -13,26 +13,31 @@
  *               Pattern is send once a second.              *
  *                                                           *
  ************************************************************/
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <syslog.h>
 #include <unistd.h>
+#include <stddef.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <sys/io.h>
 #include <math.h>
-#include <linux/laserguide.h>
+#include <linux/laser_api.h>
 #include <string.h>
 
 unsigned short lg_out_data[MAX_LG_BUFFER];
 
 int main( int argc, char ** argv )
 {
-  union  lg_info  loc_info;
+  //  union  lg_info  loc_info;
   int j, x_index, y_index;
   int  fd_laser;
   double ampl, pi, phase, jr;
 
   pi = 4.0 * atan2( 1.0, 1.0 );
-  memset(&loc_info, 0, sizeof(loc_info));
+  //  memset(&loc_info, 0, sizeof(loc_info));
   
   fd_laser = open( "/dev/laser", O_RDWR );
   if (fd_laser <= 0)
@@ -50,7 +55,9 @@ int main( int argc, char ** argv )
       ampl  = (16383 * sin( phase ) + 16384);   // 0x4000 shifts to positive wave
       lg_out_data[x_index] = lg_out_data[y_index] = (int)ampl >> 8 | ((int)ampl & 0xFF);
     }
-   // Initialize laser driver
+#if 0
+  //FIXME---PAH---NEEDS UPDATE TO NEW API
+  // Initialize laser driver
    ioctl( fd_laser, LGSTOP );
    ioctl( fd_laser, LGFASTCLOCK );
    ioctl( fd_laser, LGSTOPPULSE );
@@ -60,9 +67,10 @@ int main( int argc, char ** argv )
      {
        write(fd_laser, &lg_out_data, sizeof(lg_out_data));
        loc_info.num_points = sizeof(lg_out_data)/8;
-       ioctl(fd_laser, LGLOADWRITENUMBER, &loc_info);
+       ioctl(fd_laser, LGLDWRTCNT, &loc_info);
        ioctl(fd_laser, LGQUICKCHECK);
        sleep(1);
      }
+#endif
    exit(EXIT_SUCCESS);
 }
