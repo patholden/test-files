c9633aa72253276aba52400f30c364c0e55816ef
Kent Hartig <k.hartig@comcast.net>
2016-04-13 14:16:03 -0400

    Modified ttyloop1 and 2 to used new lgtty driver

--- ttyloop1.c
+++ ttyloop1.c
@@ -6,7 +6,9 @@
  * Test Name:  ttyloop1                                      *
  * Command  :  "ttyloop1"                                    *
  * Input    :    NONE.                                       *
- * Description:  This test exercises the kernel ttyS1 device *
+ * Description:  This test exercises the kernel laserguide   *
+ *               lgttyS1 device                              *
+ *                                                           *
  *               driver.  It writes and reads back a string  *
  *               to the device once a second.   Status is    *
  *               returned to user on success or failure.     *
@@ -39,61 +41,13 @@ int main( int argc, char ** argv )
   int     count=0, index;
   char    cdata_in=0,cdata_out=0;
   
-  fd_serial = open("/dev/ttyS1", O_RDWR | O_NONBLOCK | O_NOCTTY);
+  fd_serial = open("/dev/lgttyS1", O_RDWR | O_NONBLOCK | O_NOCTTY);
   if (fd_serial <= 0)
     {
-      perror( "Unable to open /dev/ttyS1" );
+      perror( "Unable to open /dev/lgttyS1" );
       exit(EXIT_FAILURE);
     }
 
-  // Get current settings for /dev/ttyS1
-  memset(&term, 0, sizeof(term));
-  if(tcgetattr(fd_serial, &term) < 0)
-    {
-      perror("tcgetattr0");
-      close(fd_serial);
-      exit(EXIT_FAILURE);
-    }
-
-  // Set up device to our settings
-  term.c_cflag = (term.c_cflag & ~CSIZE) | CS8; // 8 bits
-  term.c_cflag &= ~(PARENB | PARODD | CMSPAR);  // no parity 
-  term.c_cflag &= ~CSTOPB;                      // One stop bit
-  term.c_cflag &= ~(CRTSCTS);                   // Flow control off
-  term.c_iflag &= ~(IXON | IXOFF | IXANY);
-  term.c_cflag |= CLOCAL;                       // ignore modem status lines  
-  baud  = B115200;
-
-  // Disable canonical mode, and set buffer size to 1 byte
-    term.c_lflag &= ~ICANON;
-    term.c_lflag &= ~ECHO;
-    term.c_lflag &= ~ISIG;
-    term.c_cc[VMIN] = 1;
-    term.c_cc[VTIME] = 0;
-
-    // Set input & output baud rate
-    cfsetispeed( &term, baud );
-    cfsetospeed( &term, baud );
-
-    // Enable the receiver and set local mode...
-    term.c_cflag |= ( CLOCAL | CREAD );
-    term.c_cflag &= ~PARENB;
-    term.c_cflag &= ~CSTOPB;
-    term.c_cflag &= ~CSIZE;
-    term.c_cflag |= CS8;
-
-    if (tcflush(fd_serial, TCIOFLUSH))
-    {
-      perror("CantFlushttyS1");
-      close(fd_serial);
-      exit(EXIT_FAILURE);
-    }
-  if( tcsetattr( fd_serial, TCSANOW, &term) < 0 ){
-    perror("tcsetattr0");
-    close(fd_serial);
-    exit(EXIT_FAILURE);
-  }
-	
   // Initialize input & output test buffers
   test_buff_out = malloc(TEST_BUFF_LEN);
   if (!test_buff_out)
@@ -103,7 +57,7 @@ int main( int argc, char ** argv )
       exit(EXIT_FAILURE);
     }
   memset(test_buff_out, 0, TEST_BUFF_LEN);
-  sprintf(test_buff_out, "Hello from ttyS1\n");
+  sprintf(test_buff_out, "Hello from lgttyS1\n");
   test_len = strlen(test_buff_out);
 
   // Set up file-IO for serial port
@@ -124,7 +78,7 @@ int main( int argc, char ** argv )
     {
        count++;
        n = 0;
-       fprintf(stderr, "\nTTYS2 Loop-start %d", count);
+       fprintf(stderr, "\nLGTTYS1 Loop-start %d", count);
        for (index=0; index < test_len; index++)
 	 {
 	   cdata_out = test_buff_out[index];
@@ -154,12 +108,6 @@ int main( int argc, char ** argv )
 	   // Advance buffer index
 	   if ((cdata_out != 0) && (cdata_in != 0))
 	     fprintf(stderr, "\n MATCH out=%x, in=%x, index %d",cdata_out, cdata_in, index);
-	 }
-       if (tcflush(fd_serial, TCIOFLUSH))
-	 {
-	   perror("CantFlushttyS1");
-	   close(fd_serial);
-	   exit(EXIT_FAILURE);
 	 }
     }
   free(test_buff_out);
